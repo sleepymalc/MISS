7d90f7c52789b4ba6d29ae7a62fa8f53e5d234d5
Pingbang Hu <sleepymalc@gmail.com>
2024-01-11 09:44:09 +0000

    upload result

--- logistic regression/IWLS.py
+++ logistic regression/IWLS.py
@@ -6,41 +6,34 @@ from sklearn.metrics import log_loss
 from itertools import combinations
 
 def actual_effect(X_train, y_train, x_test, y_test, subset_to_remove, original_score, target="probability"):
-    # Train a Logistic Regression classifier on the reduced training set
     reduced_X_train = np.delete(X_train, subset_to_remove, axis=0)
     reduced_y_train = np.delete(y_train, subset_to_remove, axis=0)
     reduced_lr = LogisticRegression(penalty=None).fit(reduced_X_train, reduced_y_train)
 
-    # Make inference
     if target == "probability":
         reduced_score = reduced_lr.predict_proba(x_test.reshape(1, -1))[0][1]
     elif target == "train_loss":
         reduced_score = log_loss(reduced_y_train, reduced_lr.predict_proba(reduced_X_train), labels=[0, 1])
     elif target == "test_loss":
         reduced_score = log_loss([y_test], reduced_lr.predict_proba(x_test.reshape(1, -1)), labels=[0, 1])
-  
-    # Calculate the difference in predicted probabilities
+
     score_difference = reduced_score - original_score
 
     return score_difference
 
-# TODO: The actual effect of a specific k, not <= k
 def actual(X_train, y_train, x_test, y_test, k=10, job_n=50, target="probability"):
-    # Create a Logistic Regression classifier
     original_lr = LogisticRegression(penalty=None).fit(X_train, y_train)
  
-    # Initialize variables to keep track of the best subset and loss difference for parameter changes
     best_subset = np.full((k), None)
     score = []
     
     if target == "probability":
-        original_score = original_lr.predict_proba(x_test.reshape(1, -1))[0][1] # We're looking at the predicted probability of the positive class
+        original_score = original_lr.predict_proba(x_test.reshape(1, -1))[0][1] # The predicted probability of the positive class
     elif target == "train_loss":
         original_score = log_loss(y_train, original_lr.predict_proba(X_train), labels=[0, 1])
     elif target == "test_loss":
         original_score = log_loss([y_test], original_lr.predict_proba(x_test.reshape(1, -1)), labels=[0, 1])
 
-    # Loop over different subset sizes from 1 to k
     for subset_size in range(1, k + 1):
         # Generate all combinations of subsets of the current size
         subset_combinations = combinations(range(X_train.shape[0]), subset_size)
@@ -161,7 +154,7 @@ def adaptive_IWLS(X_train, y_train, x_test, y_test, k=5, target="probability"):
 
         # def sigmoid(z):
         #     return 1 / (1 + np.exp(-z))
-        
+
         # p = sigmoid(np.dot(X, coefficients))
 
         # Train to full convergence
