e48d1a496906a92f440c63fa72d966497f52ee8b
Pingbang Hu <sleepymalc@gmail.com>
2024-05-09 19:17:56 -0500

    update

--- linear/linear_regression/LAGS.py
+++ linear/linear_regression/LAGS.py
@@ -1,36 +1,36 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-def LAGS(X_train, y_train, x_test):
+def LAGS(X_train, Y_train, x_test):
     n = X_train.shape[0]
-    lr = LinearRegression().fit(X_train, y_train)
+    lr = LinearRegression().fit(X_train, Y_train)
 
     N_inv = np.linalg.inv(X_train.T @ X_train)
     H = X_train @ N_inv @ X_train.T
-    influence = ((x_test @ N_inv @ X_train.T).reshape(-1, 1) * (lr.predict(X_train) - y_train)).reshape(-1) / (1 - np.diag(H))
+    influence = ((x_test @ N_inv @ X_train.T).reshape(-1, 1) * (lr.predict(X_train) - Y_train)).reshape(-1) / (1 - np.diag(H))
 
     LAGS_best = np.argsort(influence)[-n:][::-1]
 
     return LAGS_best
 
-def adaptive_LAGS(X_train, y_train, x_test, k=5):
+def adaptive_LAGS(X_train, Y_train, x_test, k=5):
     n = X_train.shape[0]
     index = list(range(n))
     adaptive_LAGS_best_k = np.zeros(k, dtype=int)
 
     for i in range(k):
-        lr = LinearRegression().fit(X_train, y_train)
+        lr = LinearRegression().fit(X_train, Y_train)
 
         N_inv = np.linalg.inv(X_train.T @ X_train)
         H = X_train @ N_inv @ X_train.T
-        influence = ((x_test @ N_inv @ X_train.T).reshape(-1, 1) * (lr.predict(X_train) - y_train)).reshape(-1) / (1 - np.diag(H))
+        influence = ((x_test @ N_inv @ X_train.T).reshape(-1, 1) * (lr.predict(X_train) - Y_train)).reshape(-1) / (1 - np.diag(H))
 
         top_index = np.argsort(influence)[-1:][::-1][0]
         adaptive_LAGS_best_k[i] = index[top_index]
 
         # Remove the most influential data points
         X_train = np.delete(X_train, top_index, axis=0)
-        y_train = np.delete(y_train, top_index, axis=0)
+        Y_train = np.delete(Y_train, top_index, axis=0)
         index = np.delete(index, top_index, axis=0)
 
     return adaptive_LAGS_best_k
--- linear/logistic_regression/first_order.py
+++ linear/logistic_regression/first_order.py
@@ -1,9 +1,9 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
-def first_order(X_train, y_train, x_test, y_test):
+def first_order(X_train, Y_train, x_test, y_test):
     n = X_train.shape[0]
-    lr = LogisticRegression(penalty=None).fit(X_train, y_train)
+    lr = LogisticRegression(penalty=None).fit(X_train, Y_train)
 
     X_train_bar = np.hstack((np.ones((X_train.shape[0], 1)), X_train))
 
@@ -12,11 +12,11 @@ def first_order(X_train, y_train, x_test, y_test):
 
     # Compute the gradient of the logistic loss w.r.t. the parameters
     sigma_train = lr.predict_proba(X_train)[:, 1] # P(1 | x)
-    grad_loss_train = (sigma_train - y_train) * X_train_bar.T
+    grad_loss_train = (sigma_train - Y_train) * X_train_bar.T
     param_influence = Hessian_inv @ grad_loss_train / n
 
     x_test_bar = np.hstack((1, x_test))
-    sigma_test = lr.predict_proba(x_test.reshape(1, -1))[0][1]
+    sigma_test = lr.predict_proba(x_test.reshape(1, -1))[0][y_test]
     phi = (1 - sigma_test) * sigma_test * x_test_bar
 
     influence = (phi @ param_influence).reshape(-1)
@@ -25,24 +25,24 @@ def first_order(X_train, y_train, x_test, y_test):
 
     return FO_best
 
-def adaptive_first_order(X_train, y_train, x_test, y_test, k=5):
+def adaptive_first_order(X_train, Y_train, x_test, y_test, k=5):
     n = X_train.shape[0]
     X_train_bar = np.hstack((np.ones((X_train.shape[0], 1)), X_train))
     index = list(range(n))
     adaptive_FO_best_k = np.zeros(k, dtype=int)
 
     for i in range(k):
-        lr = LogisticRegression(penalty=None).fit(X_train, y_train)
+        lr = LogisticRegression(penalty=None).fit(X_train, Y_train)
 
         Hessian_inv = np.linalg.inv(np.dot(X_train_bar.T, np.dot(np.diag(lr.predict_proba(X_train)[:, 1] * (1 - lr.predict_proba(X_train)[:, 1])), X_train_bar)) / n)
 
         # Compute the gradient of the logistic loss w.r.t. the parameters
         sigma_train = lr.predict_proba(X_train)[:, 1] # P(1 | x)
-        grad_loss_train = (sigma_train - y_train) * X_train_bar.T
+        grad_loss_train = (sigma_train - Y_train) * X_train_bar.T
         param_influence = Hessian_inv @ grad_loss_train / n
 
         x_test_bar = np.hstack((1, x_test))
-        sigma_test = lr.predict_proba(x_test.reshape(1, -1))[0][1]
+        sigma_test = lr.predict_proba(x_test.reshape(1, -1))[0][y_test]
         phi = (1 - sigma_test) * sigma_test * x_test_bar
 
         influence = (phi @ param_influence).reshape(-1)
@@ -53,7 +53,7 @@ def adaptive_first_order(X_train, y_train, x_test, y_test, k=5):
         # Remove the most influential data points
         X_train = np.delete(X_train, top_index, axis=0)
         X_train_bar = np.delete(X_train_bar, top_index, axis=0)
-        y_train = np.delete(y_train, top_index, axis=0)
+        Y_train = np.delete(Y_train, top_index, axis=0)
         index = np.delete(index, top_index, axis=0)
 
     return adaptive_FO_best_k
