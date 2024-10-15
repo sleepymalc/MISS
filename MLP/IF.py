73bc60d8d741d8f22ef4c58826ab5299351f74d6
sleepymalc <sleepymalc@gmail.com>
2024-10-15 13:20:30 -0500

    First Release

--- MLP/IF.py
+++ MLP/IF.py
@@ -3,7 +3,7 @@ from pydvl.influence.torch import EkfacInfluence
 from utlis.grad_calculator import count_parameters, grad_calculator
 from utlis.data import data_generation
 from tqdm import tqdm
-import re
+import copy
 
 class MISS_IF:
     def __init__(self,
@@ -11,30 +11,36 @@ class MISS_IF:
                  model_checkpoints,
                  train_loader,
                  test_loader,
+                 ensemble,
                  model_output_class,
+                 seed,
                  device):
         '''
         :param model: a nn.module instance, no need to load any checkpoint
         :param model_checkpoints: a list of checkpoint path, the length of this list indicates the ensemble model number
         :param train_loader: train samples in a data loader
-        :param train_loader: test samples in a data loader
+        :param test_loader: test samples in a data loader
+        :param ensemble: ensemble number
         :param model_output_class: a class definition inheriting BaseModelOutputClass
+        :param seed: previous experiment's seed
         :param device: the device running
         '''
         self.model = model
 
         self.model_checkpoints = model_checkpoints
-        self.model_checkpoints_cpy = model_checkpoints
+        self.model_checkpoints_cpy = copy.deepcopy(model_checkpoints)
 
         self.train_loader = train_loader
-        self.train_loader_cpy = train_loader
+        self.train_loader_cpy = copy.deepcopy(train_loader)
 
         self.test_loader = test_loader
-        self.test_loader_cpy = test_loader
+        self.test_loader_cpy = copy.deepcopy(test_loader) # We won't touch this but just to be safe
+
+        self.ensemble = ensemble
 
         self.model_output_class = model_output_class
+        self.seed = seed
         self.device = device
-        self.warm_start = True
 
     def _convert_from_loader(self, loader):
         data = [(features, labels) for features, labels in loader]
@@ -74,64 +80,20 @@ class MISS_IF:
 
         # Calculate the indices of top k influential samples for each test sample
         test_size = len(self.test_loader)
-        MISS = torch.zeros(test_size, k, dtype=torch.int32)
+        MIS = torch.zeros(test_size, k, dtype=torch.int32)
 
-        print("Start TRAK greedy")
         for i in range(test_size):
-            MISS[i, :] = torch.topk(influence_mean[i], k).indices
+            MIS[i, :] = torch.topk(influence_mean[i], k).indices
 
         self._reset()
-        return MISS
-
-    # def adaptive_most_k(self, k, warm_start=True, step_size=5):
-    #     test_size = len(self.test_loader)
-    #     train_size = len(self.train_loader)
-    #     MIS = torch.zeros(test_size, k, dtype=torch.int32)
-
-    #     for i in tqdm(range(test_size)):
-    #         index = list(range(train_size))
-    #         step = step_size
-    #         for j in range(0, k, step_size):
-    #             # handle overflow of j+step
-    #             if j + step > k:
-    #                 step = k - j
-
-    #             self.train_loader, _ = data_generation([l for l in range(train_size) if l not in MIS[i, :j]], list(range(test_size)), mode='MISS')
-
-    #             max_idx_list = self.most_k(step)[i, :]
-    #             MIS[i, j:j+step] = torch.tensor([index[l] for l in max_idx_list])
-    #             index = [index[l] for l in range(len(index)) if l not in max_idx_list]
-
-    #             # update the model, the dataset
-    #             train_loader, _ = data_generation([l for l in range(train_size) if l not in MIS[i, :j+step]], list(range(test_size)), mode='train')
-
-    #             for idx, checkpoint_file in enumerate(self.model_checkpoints):
-    #                 if warm_start:
-    #                     self.model.load_state_dict(torch.load(checkpoint_file))
-    #                     self.model.train_with_seed(train_loader, epochs=8, seed=idx, verbose=False)
-    #                     torch.save(self.model.state_dict(), f"./checkpoint/adaptive_tmp/seed_{self.seed}_ensemble_{idx}_w.pt")
-    #                 else:
-    #                     self.model = self.model_cpy
-    #                     self.model.train_with_seed(train_loader, epochs=30, seed=idx, verbose=False)
-    #                     torch.save(self.model.state_dict(), f"./checkpoint/adaptive_tmp/seed_{self.seed}_ensemble_{idx}.pt")
-
-    #             if j == 0:
-    #                 if warm_start:
-    #                     self.model_checkpoints = [f"./checkpoint/adaptive_tmp/seed_{self.seed}_ensemble_{idx}_w.pt" for idx in range(self.ensemble)]
-    #                 else:
-    #                     self.model_checkpoints = [f"./checkpoint/adaptive_tmp/seed_{self.seed}_ensemble_{idx}.pt" for idx in range(self.ensemble)]
-    #         self._reset()
-
-    #     return MIS
+        return MIS
 
     def adaptive_most_k(self, k, warm_start=True, step_size=5):
         test_size = len(self.test_loader)
         train_size = len(self.train_loader)
-        ensemble_num = len(self.model_checkpoints)
-        seed = int(re.search(r'seed_(\d+)_ensemble_(\d+)', self.model_checkpoints[0]).group(1))
-        MISS = torch.zeros(test_size, k, dtype=torch.int32)
+        MIS = torch.zeros(test_size, k, dtype=torch.int32)
 
-        for j in tqdm(range(test_size)):
+        for i in tqdm(range(test_size)):
             index = list(range(train_size))
             step = step_size
             for j in range(0, k, step_size):
@@ -146,17 +108,17 @@ class MISS_IF:
                 index = [index[i] for i in range(len(index)) if i not in max_idx_list]
 
                 # update the model, the dataset
-                train_loader, _ = data_generation([i for i in range(train_size) if i not in MISS[j, :k]], list(range(test_size)), mode='train')
+                train_loader, _ = data_generation([l for l in range(train_size) if l not in MIS[i, :j+step]], list(range(test_size)), mode='train')
 
                 for idx, checkpoint_file in enumerate(self.model_checkpoints):
-                    if self.warm_start:
+                    if warm_start:
                         self.model.load_state_dict(torch.load(checkpoint_file))
-                        epochs = 8
+                        self.model.train_with_seed(train_loader, epochs=8, seed=idx, verbose=False)
+                        torch.save(self.model.state_dict(), f"./checkpoint/adaptive_tmp/seed_{self.seed}_ensemble_{idx}_w.pt")
                     else:
                         self.model = self.model_cpy
-                        epochs = 30
-                    self.model.train_with_seed(train_loader, epochs=epochs, seed=idx, verbose=False)
-                    torch.save(self.model.state_dict(), f"./checkpoint/tmp/seed_{seed}_{idx}.pt")
+                        self.model.train_with_seed(train_loader, epochs=30, seed=idx, verbose=False)
+                        torch.save(self.model.state_dict(), f"./checkpoint/adaptive_tmp/seed_{self.seed}_ensemble_{idx}.pt")
 
                 if warm_start:
                     self.model_checkpoints = [f"./checkpoint/adaptive_tmp/seed_{self.seed}_ensemble_{idx}_w.pt" for idx in range(self.ensemble)]
@@ -164,4 +126,4 @@ class MISS_IF:
                     self.model_checkpoints = [f"./checkpoint/adaptive_tmp/seed_{self.seed}_ensemble_{idx}.pt" for idx in range(self.ensemble)]
             self._reset()
 
-        return MISS
+        return MIS
--- MLP/MISS.py
+++ MLP/MISS.py
@@ -8,19 +8,32 @@ import argparse
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 
+def parse_range(value):
+    try:
+        start, end = map(int, value.split(':'))
+        if start < 0 or end < 0 or start > end:
+            raise argparse.ArgumentTypeError("Invalid range: start should be less or equal than end and both should be non-negative")
+        return start, end
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid range format: expected 'start:end'")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train_size", type=int, default=5000, help="train dataset size")
-    parser.add_argument("--test_size", type=int, default=50, help="test dataset size")
-    parser.add_argument("--ensemble", type=int, default=5, help="ensemble number")
     parser.add_argument("--seed", type=int, default=0, help="seed")
+    parser.add_argument("--train_size", type=int, default=5000, help="train dataset size")
+    parser.add_argument("--test_range", type=parse_range, required=True, help="test dataset range. Format: 'start:end'")
+    parser.add_argument("--ensemble", type=int, default=5, help="ensemble number")
     parser.add_argument("--k", type=int, default=50, help="size of the most influential subset")
-    parser.add_argument("--step", type=int, default=5, help="step size for adaptive influence function")
+    parser.add_argument("--adaptive", action='store_true', help="use adaptive greedy")
+    parser.add_argument("--warm_start", action='store_true', help="enable warm start for adaptive greedy")
+    parser.add_argument("--step", type=int, default=5, help="step size for adaptive greedy")
     args = parser.parse_args()
 
     torch.manual_seed(args.seed)
 
-    train_loader, test_loader = data_generation(list(range(args.train_size)), list(range(args.test_size)), mode='MISS')
+    test_start_idx, test_end_idx = args.test_range
+
+    train_loader, test_loader = data_generation(list(range(args.train_size)), list(range(test_start_idx, test_end_idx + 1)), mode='MISS')
 
     checkpoint_files = [f"./checkpoint/seed_{args.seed}_ensemble_{i}.pt" for i in range(args.ensemble)]
 
@@ -28,12 +41,17 @@ if __name__ == "__main__":
                  model_checkpoints=checkpoint_files,
                  train_loader=train_loader,
                  test_loader=test_loader,
+                 ensemble=args.ensemble,
                  model_output_class=MNISTModelOutput,
+                 seed=args.seed,
                  device=device)
 
-    # Retrain the model without the most influential samples for every test point
-    MISS = IF.most_k(args.k)
-    torch.save(MISS, f"./results/IF/seed_{args.seed}_k_{args.k}_ensemble_{args.ensemble}.pt")
-
-    MISS = IF.adaptive_most_k(args.k, step_size=args.step)
-    torch.save(MISS, f"./results/IF/seed_{args.seed}_k_{args.k}_ensemble_{args.ensemble}_adaptive_step_{args.step}.pt")
+    if not args.adaptive: # MISS with naive greedy
+        MIS = IF.most_k(args.k)
+        torch.save(MIS, f"./results/IF/seed_{args.seed}_k_{args.k}_ensemble_{args.ensemble}_test_{test_start_idx}-{test_end_idx}.pt")
+    else: # MISS with adaptive greedy
+        MIS = IF.adaptive_most_k(args.k, warm_start=args.warm_start, step_size=args.step)
+        if args.warm_start:
+            torch.save(MIS, f"./results/IF/seed_{args.seed}_k_{args.k}_ensemble_{args.ensemble}_adaptive_step_{args.step}_warm-start_test_{test_start_idx}-{test_end_idx}.pt")
+        else:
+            torch.save(MIS, f"./results/IF/seed_{args.seed}_k_{args.k}_ensemble_{args.ensemble}_adaptive_step_{args.step}_test_{test_start_idx}-{test_end_idx}.pt")
